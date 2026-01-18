7d35725047f95e382626c9de78579ba5116f3988
adityayadav-codes <adityayadav2905@gmail.com>
2026-01-18 23:21:53 +0530

    A program to demonstrate Even Or odd number

--- Prog.java
+++ Prog.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class Prog {
-    
+    public static void main(String[] args) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        x = sc.nextInt();
+        if(x % 2 == 0) {
+            System.out.println(x + " is even.");
+        } else {
+            System.out.println(x + " is odd.");
+        }
+        sc.close();
+    }
 }
