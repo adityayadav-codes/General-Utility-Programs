4179ce47bf6f51794727629e0cc088aaf14d9c7e
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-18 23:07:25 +0530

    Find Duplicate Elements in an array

---
+++ 18Mar/JavProg.java
@@ -0,0 +1,19 @@
+public class JavProg {
+
+    public static void main(String [] args) {
+
+        int [] arr = {10,10,40,50,20,40};
+
+        for(int i =0; i < arr.length; i++) {
+
+            for(int j = i + 1; j < arr.length; j++) {
+
+                if(arr[i] == arr[j]) {
+
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
