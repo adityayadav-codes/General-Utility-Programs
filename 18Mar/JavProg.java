1bb4b64093c31b87ac694d3ef1a5d774569f7a6f
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-18 23:27:22 +0530

    Count Vowels in a String

--- 18Mar/JavProg.java
+++ 18Mar/JavProg.java
@@ -1,19 +1,41 @@
+
+
+// public class JavProg {
+
+//     public static void main(String [] args) {
+
+//         int [] arr = {10,10,40,50,20,40};
+
+//         for(int i =0; i < arr.length; i++) {
+
+//             for(int j = i + 1; j < arr.length; j++) {
+
+//                 if(arr[i] == arr[j]) {
+
+//                     System.out.println(arr[i]);
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Count Vowels in a Srirng ===================================
+
 public class JavProg {
 
     public static void main(String [] args) {
 
-        int [] arr = {10,10,40,50,20,40};
+        String str = "icecrean";
+        int count = 0;
 
-        for(int i =0; i < arr.length; i++) {
+        for(int i = 0; i < str.length(); i++) {
 
-            for(int j = i + 1; j < arr.length; j++) {
+            if(str.charAt(i)== 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
 
-                if(arr[i] == arr[j]) {
-
-                    System.out.println(arr[i]);
-                    break;
-                }
+                count++;
             }
         }
+        System.out.println(count);
     }
 }
