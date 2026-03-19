e51a6177d7916ecdd164487d85aedaefdfbe819a
adityayadav-codes <adityayadav2905@gmail.com>
2026-03-19 16:04:41 +0530

    Merge Two arrays using simple method

--- 19mar/Demo.java
+++ 19mar/Demo.java
@@ -1,20 +1,55 @@
 import java.util.*;
 // Removes Duplicates From Array 
 
-public class Demo {
+// public class Demo {
 	
-	public static void main(String [] args) {
+// 	public static void main(String [] args) {
 
-		int [] arr = {1,2,3,2,4,1};
+// 		int [] arr = {1,2,3,2,4,1};
 
-		HashSet <Integer> set = new HashSet<>();
+// 		HashSet <Integer> set = new HashSet<>();
 
-		for(int i : arr) {
+// 		for(int i : arr) {
 
-			set.add(i);
+// 			set.add(i);
+// 		}
+
+// 		System.out.println(set);
+// 	}
+// }
+
+// Merger Arrays using simple method 
+
+public class Demo {
+
+	 public static void main(String[] args) {
+		
+		int arr1 [] = {10,20,30};
+		int arr2 [] = {40,50,60};
+
+		System.out.println("Before Merging:");
+		for(int i : arr1) System.out.print(i + " ");
+
+		System.out.println();
+
+		for(int i : arr2) System.out.print(i + " ");
+		int [] result = new int[arr1.length + arr2.length];
+
+		// copy arr1
+		for(int i =0; i < arr1.length; i++) {
+
+			result[i] = arr1[i];
 		}
+		 // copy arr2
+		 for(int i =0; i < arr2.length; i++) {
 
-		System.out.println(set);
-	}
+			result[arr1.length + i] = arr2[i];
+		 }
+		 System.out.println();
+		System.out.println("After Merging: ");
+		for(int num : result) {
+
+			System.out.print(num + " ");
+		}
+	 }
 }
-
